2d7a1509f3f0a3c1a2bdad3497c1f901c9103efb
teju0519 <teju0516@gmail.com>
2024-05-02 18:47:30 -0300

    added modules

--- main.tf
+++ main.tf
@@ -11,90 +11,59 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example_rg" {
-  name     = "${var.prefix}-rg"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "example_vnet" {
-  name                = "${var.prefix}-network"
-  resource_group_name = azurerm_resource_group.example_rg.name
-  location            = azurerm_resource_group.example_rg.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "example_subnet" {
-  name                 = "${var.prefix}-subnet"
-  resource_group_name  = azurerm_resource_group.example_rg.name
-  virtual_network_name = azurerm_virtual_network.example_vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "example_pip" {
-  name = "acceptancetestpublicip1"
-  resource_group_name = azurerm_resource_group.example_rg.name
-  location = azurerm_resource_group.example_rg.location
-  allocation_method = "Static"
-}
-
-resource "azurerm_network_interface" "example_ni" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example_rg.location
-  resource_group_name = azurerm_resource_group.example_rg.name
-
-  ip_configuration {
-    name                          = "testipc"
-    private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.example_subnet.id
-    public_ip_address_id = azurerm_public_ip.example_pip.id
+variable "prefix" {
+  description = "name of resource group"
+  type = map(string)
+  default = {
+    "dev" = "tejadevsg"
+    "stage" = "tejastagesg"
+    "prod" = "tejaprodsg"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example_vm" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example_rg.location
-  resource_group_name   = azurerm_resource_group.example_rg.name
-  admin_username = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.example_ni.id,
-  ]
-  size               = "Standard_DS1_v2"
-  admin_ssh_key {
-    username = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-  os_disk {
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  tags = {
-    environment = "staging"
-  }
+module "resourcegreouptest" {
+  source = "./modules/vm"
+  prefix = lookup(var.prefix, terraform.workspace, "tejadefaultsg")
 }
 
-resource "azurerm_storage_account" "example_sa" {
-  name = "${var.prefix}sa"
-  resource_group_name = azurerm_resource_group.example_rg.name
-  location = azurerm_resource_group.example_rg.location
-  account_tier = "Standard"
-  account_replication_type = "LRS"
+
+
+
+
+
+
+
+
+example for vault code
+
+provider "aws" {
+    region = "us-east-1"
 }
 
-resource "azurerm_storage_container" "example_sc" {
-  name = "content"
-  storage_account_name = azurerm_storage_account.example_sa.name
-  container_access_type = "private"
+provider "vault" {
+    address = "10.2.3.4:4056"
+    skip_child_token = true
+    auth_login {
+        path = "auth/approle/login"
+        parameters = {
+          role_id = "fsdf"
+          secret_id = "dfgdfhg"
+        }
+    }
 }
 
-resource "azurerm_storage_blob" "example_blob" {
-  name = "mycontent"
-  storage_account_name = azurerm_storage_account.example_sa.name
-  storage_container_name = azurerm_storage_container.example_sc.name
-  type = "Block"
+data "vault_kv_secret_v2" "example" {
+    mount = "kv"
+    name = "test-secret"
 }
+
+resource "aws_instance" "new" {
+    ami = "ami-34346456456"
+    tags = {
+      secret = data.vault_kv_secret_v2.example.data["username"]
+      name = "sampleec2instance"
+      environment = "production"
+    }
+}
+
+
