a3faf6c2fcfd0dbc8524c92269c8edcb232fe999
teju0519 <teju0516@gmail.com>
2024-05-02 22:24:45 -0300

    adding modules folder

---
+++ modules/vm/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example_rg" {
+  name     = "${var.prefix}-rg"
+  location = "East US"
+}
